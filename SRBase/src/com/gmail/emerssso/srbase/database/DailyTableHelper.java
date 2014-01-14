324389c9ac61f340fb0ba966df5a19974d5c0bb9
Conner Kasten <emerssso@gmail.com>
2014-01-14 14:39:53 -0500

    Add Jautodoc to DailyTableHelper

--- SRBase/src/com/gmail/emerssso/srbase/database/DailyTableHelper.java
+++ SRBase/src/com/gmail/emerssso/srbase/database/DailyTableHelper.java
@@ -4,20 +4,38 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DailyTableHelper.
+ */
 public class DailyTableHelper extends SQLiteOpenHelper {
 	
+	/** The Constant DATABASE_NAME. */
 	private static final String DATABASE_NAME = "dailytable.db";
+	
+	/** The Constant DATABASE_VERSION. */
 	private static final int DATABASE_VERSION = 1;
 
+	/**
+	 * Instantiates a new daily table helper.
+	 *
+	 * @param context the context
+	 */
 	public DailyTableHelper(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 	
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase database) {
 		DailyTable.onCreate(database);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase database, 
 			int oldVersion, int newVersion) {
