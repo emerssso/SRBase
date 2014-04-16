76c466364a7d3d8ee3e683f2190b3f3aaa248e00
Conner Kasten <emerssso@gmail.com>
2014-04-16 13:38:26 -0700

    Revert "Show dialog while upgrading Database"
    This reverts commit e7097bf2c2bf471db24364505801e97d65e8b995.

--- SRBase/src/com/gmail/emerssso/srbase/database/SRDatabaseHelper.java
+++ SRBase/src/com/gmail/emerssso/srbase/database/SRDatabaseHelper.java
@@ -2,11 +2,9 @@
 //The License is available at http://www.apache.org/licenses/LICENSE-2.0
 package com.gmail.emerssso.srbase.database;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.AsyncTask;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -21,12 +19,6 @@ public class SRDatabaseHelper extends SQLiteOpenHelper {
 	/** The Constant DATABASE_VERSION. */
 	private static final int DATABASE_VERSION = 2;
 	
-	private Context ctx;
-	private SQLiteDatabase database;
-	private int oldVersion;
-	private int newVersion;
-	
-	
 	/**
 	 * Instantiates a new sR table helper.
 	 *
@@ -34,7 +26,6 @@ public class SRDatabaseHelper extends SQLiteOpenHelper {
 	 */
 	public SRDatabaseHelper(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		ctx = context;
 	}
 
 	/* (non-Javadoc)
@@ -53,39 +44,9 @@ public class SRDatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase database, 
 			int oldVersion, int newVersion) {
-		this.database = database;
-		this.oldVersion = oldVersion;
-		this.newVersion = newVersion;
-		new UpgradeAsyncTask().execute();
-	}
-	
-	private void callUpgrades() {
 		SRTable.onUpgrade(database, oldVersion, newVersion);
 		DailyTable.onUpgrade(database, oldVersion, newVersion);
 		PartTable.onUpgrade(database, oldVersion, newVersion);
 	}
 
-	class UpgradeAsyncTask extends AsyncTask<Void, Void, Void> {
-
-	    private ProgressDialog progressDialog;
-
-	    @Override
-	    protected void onPreExecute() {
-	        progressDialog = ProgressDialog.show(ctx, "Upgrading Your Database", 
-	        		"Please wait a moment while your data is migrated", 
-	        		true, false);
-	    }
-
-	    @Override
-	    protected Void doInBackground(Void... params) {        
-	        callUpgrades();
-	        return null;
-	    }
-
-	    @Override
-	    protected void onPostExecute(Void result) {
-	        progressDialog.dismiss();
-	    }
-	}
-	
 }
